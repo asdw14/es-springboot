a37f43d8265843128af4411987506e17fa7f7bdd
dizhongdi <2755063993@qq.com>
2022-07-16 23:57:03 +0800

    Initial commit

---
+++ src/test/java/com/dizhongdi/esspringboot/EsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizhongdi.esspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
